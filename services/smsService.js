a9a0240b2546eb7effd2d309ddbe9933a2734a0e
Suraj Sharma <surajkrsharma207@gmail.com>
2026-04-09 20:13:48 +0530

    Edit Frofile and Profile

--- services/smsService.js
+++ services/smsService.js
@@ -1,47 +1,19 @@
-// import SmsAndroid from "react-native-get-sms-android";
-
-// export const sendSMS = (contacts, message) => {
-//   contacts.forEach((number) => {
-//     SmsAndroid.autoSend(
-//       number,
-//       message,
-//       (fail) => console.log("SMS Failed:", fail),
-//       (success) => console.log("SMS Sent:", success)
-//     );
-//   });
-// };
-
 import { Platform } from "react-native";
-import * as SMS from "expo-sms";
-import Constants from "expo-constants";
 
 export const sendSMS = async (contacts, message) => {
-  // 🟢 EXPO GO (DEV MODE)
-  if (Constants.appOwnership === "expo") {
-    console.log("📱 Using Expo SMS");
+  if (Platform.OS === "android") {
+    // Native SMS (AUTO SEND)
+    const SmsAndroid = require("react-native-get-sms-android");
 
-    const available = await SMS.isAvailableAsync();
-
-    if (!available) {
-      console.log("SMS not available");
-      return;
-    }
-
-    await SMS.sendSMSAsync(contacts, message);
-    return; 
+    contacts.forEach((number) => {
+      SmsAndroid.autoSend(
+        number,
+        message,
+        (fail) => console.log("SMS Failed:", fail),
+        (success) => console.log("SMS Sent:", success)
+      );
+    });
+  } else {
+    console.log("SMS only works on Android");
   }
-
-  //PRODUCTION (APK)
-  console.log("Using Native SMS");
-
-  //const SmsAndroid = require("react-native-get-sms-android");
-
-//   contacts.forEach((number) => {
-//     SmsAndroid.autoSend(
-//       number,
-//       message,
-//       (fail) => console.log("SMS Failed:", fail),
-//       (success) => console.log("SMS Sent:", success)
-//     );
-//   });
 };
